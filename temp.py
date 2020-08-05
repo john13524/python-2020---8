32fddc6a50295c6509ff2bd8ff06b6484ecedd45
john13524 <69131811+john13524@users.noreply.github.com>
2020-08-05 15:35:32 +0800

    Add files via upload

--- temp.py
+++ temp.py
@@ -1,24 +1,66 @@
-import turtle
-c = 90
-b = turtle.Turtle()
-a = turtle.Turtle()
-b.color('red')
-b.shape('turtle')
-a.color('green')
-a.shape('turtle')
-a.pensize(11)
-b.pensize(11)
-for i in range(5):
-    for i in range(12):
-        b.right(c)
-        b.forward(100)
-        a.left(c)
-        a.forward(100)
-        b.stamp()
-        a.stamp()
-    for i in range(12):
-        b.left(c)
-        b.forward(100)
-        a.right(c)
-        a.forward(100)
-turtle.done()
+import os.path
+a = {}
+if not os.path.isfile('md.txt'):
+    of = open('md.txt','w')
+    print('new file')
+else:
+    of = open('md.txt','r')
+    print('old file')
+for row in of.readlines():
+    date = row.split(':')
+    key = date[0]
+    value = date[1]
+    value = value.split()
+    a[key]=value
+print(a)
+     
+    
+    
+
+    
+    
+while True:
+    print('1.建立字串')
+    print("2.打出全部資料")
+    print('3.英翻中')
+    print('4.中翻英')
+    print('5.考試')
+    print('6.離開')
+    b = input('輸入功能:')
+    if b =='1':
+        d = input('輸入中文:')
+        c = input('輸入英文:')
+        a[c]=d
+        fo = open('md.txt','w')
+        for k,v in d.items():
+            of.write(k)
+            of.write(':')
+            of.write(v)
+            of.write('\n')
+    elif b =="2":
+        for k,v in a.items():
+            print(k,':',v)
+    elif b == '3':
+        f = input('輸入想翻譯的英文:')
+        print(a[f])
+    elif b == '4':
+        f = input('輸入想翻譯的中文:')
+        for k,v in a.items():
+            if f == v:
+                print(k)
+    elif b == '5':
+        F = 0
+        for k,v in a.items():
+            print(v,':')
+            A = input('輸入答案')
+            if A ==k:
+                F = F + 1
+                print('對')
+        print('你的分數',F)
+    elif b == '6':
+        print('下班耶')
+        print('看3小')
+        print('error')
+        for i in range(100000):
+            print("errorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerrorerror")
+        break
--- untitled0.py
+++ untitled0.py
@@ -1,7 +1,7 @@
-print("""lkjrwehtifueywsfr84er7tu954etuy98e87erw5uy98745763t984er6ytiufdrhguieratgluthfsiugkyesa6gyiutd6yufdi7iuldlxux;
-      hdslkjfhjdsjkhfjdgfkjhsjfkjdshfkjdhskjfhdshfjkhdfjdshfjdshkjfhdsjfhdshfjdskhfkjdshfkjdshfjdshfjdshfjdshfjkdshfkjhds
-      kdjfdshfkdshfkjhdjskfkjdshfkjdshfkjdshfkjdshfkjdshfjdshfkjdshfkjdshbfkjdshfbdsfkjdsnbfkjsdhfkjsdhfkjdshfjkdshfkjdshfkjdsh
-      fldsjfdsjfkldsjflkjdsklfkdsfjdjfkdsjflkdsjfdlksjflkdsjfkdsjfksdjkfjdskfjkdsfjkldsjfkdsjfkldsjfkldsjflkdsjl
-      jfjfjfjfjfjjfjfkdsjfdsfkdsjlkfjdslkfjlkdsjflkdsjgflkfdshlkjgfh lkfdshglkjbtsakukukukukuihtlrneytwaciuecwyociewwoyecaoy
-      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa980er7wayrbvcoiewayrioycycycycycycycycycycycycycycycycycycycycycycycycycycycycycycycycycycycyci
-      kjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfckjfcI""")
+f = open('sans1.jpg','rb')
+img = f.read()
+print(img)
+f.close()
+copy = open('copy.jpg','wb')
+copy.write(img)
+copy.close()
