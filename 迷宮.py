4f9dbca86db4eec633276f32288520ff2ce3b446
john13524 <69131811+john13524@users.noreply.github.com>
2020-08-06 16:56:34 +0800

    Add files via upload

---
+++ 迷宮.py
@@ -0,0 +1,32 @@
+import random
+import turtle
+a = turtle.Turtle()
+b = turtle.Turtle()
+a.speed(10)
+a.penup()
+a.forward(400)
+a.pendown()
+a.left(90)
+a.forward(400)
+a.left(90)
+a.forward(800)
+a.left(90)
+a.forward(390)
+a.penup
+a.forward(20)
+a.pendown
+a.forward(390)
+a.left(90)
+a.forward(800)
+a.left(90)
+a.forward(380)
+a.penup()
+b.penup()
+a.goto(-400,0)
+b.goto(-400,20)
+a.right(90)
+   
+
+turtle.done()
+
+    
